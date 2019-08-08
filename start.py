d5b6c5f19ed9447d7644df5675bfa9d5e9191306
Jake Cariello <jec91@duke.edu>
2019-08-08 17:17:57 -0400

    fiber manager and SAVING STATES!! HYPE!

--- start.py
+++ start.py
@@ -20,7 +20,7 @@ runner = Runner(master_config_file_path)
 # runner.reposition_test()
 # runner.reposition_test2()
 # runner.manager_test()
-runner.load_up_manager()
+runner.smart_run()
 
 # END timer
 end = time.time()
