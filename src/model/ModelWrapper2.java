55e72eb7338e761cbb6c278ed7a0270d41e00f30
Jake Cariello <jec91@duke.edu>
2019-09-29 12:49:52 -0400

    mw.save

--- src/model/ModelWrapper2.java
+++ src/model/ModelWrapper2.java
@@ -2,6 +2,7 @@ package model;
 
 import com.comsol.model.Model;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -28,24 +29,42 @@ public class ModelWrapper2 {
         this.dest = defaultSaveDestination;
     }
 
-    public next(String key, String pseudonym) {
-        String id = this.next(key);
-        identifierPseudonyms.put(id, pseudonym);
-    }
-
-    public String next(String key) {
+    public String nextID(String key) {
         // default next index to 1 (assume first call of key)
         int nextIndex = 1;
 
         // if the key already exists, set
-        if (identifierStates.containsKey(key)) {
-            nextIndex = identifierStates.get(key) + 1;
-        }
+        if (identifierStates.containsKey(key)) nextIndex = identifierStates.get(key) + 1;
 
         // update identifiers index
         identifierStates.put(key, nextIndex);
-
-        // return String version (i.e. "keyN")
         return key + nextIndex;
     }
+
+    public String nextID(String key, String pseudonym) {
+        // get next key using base method
+        String id = this.nextID(key);
+        // put into map as key, value pair
+        identifierPseudonyms.put(id, pseudonym);
+        return id;
+    }
+
+    public boolean save(String destination) {
+        try {
+            this.model.save(destination);
+            return true;
+        } catch (IOException e) {
+            System.out.println("Failed to save to destination: " + destination);
+            return false;
+        }
+    }
+
+    public boolean save() {
+        if (this.dest != null) return save(this.dest);
+        else {
+            System.out.println("Save directory not initialized");
+            return false;
+        }
+    }
+
 }
