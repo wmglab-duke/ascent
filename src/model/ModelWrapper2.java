b32066b134cb9f3b376bfe55415efde417e3321f
Eric Musselman <edm23@duke.edu>
2019-09-29 12:39:41 -0400

    NEXT!

--- src/model/ModelWrapper2.java
+++ src/model/ModelWrapper2.java
@@ -28,5 +28,21 @@ public class ModelWrapper2 {
         this.dest = defaultSaveDestination;
     }
 
+    public String next(String key) {
+        // default next index to 1 (assume first call of key)
+        int nextIndex = 1;
+
+        // if the key already exists, set
+        if (identifierStates.containsKey(key)) {
+            nextIndex = identifierStates.get(key) + 1;
+        }
+
+        // update identifiers index
+        identifierStates.put(key, nextIndex);
+
+        // return String version (i.e. "keyN")
+        return key + nextIndex;
+    }
+
 
 }
