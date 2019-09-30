250a0256d1075991f194cd5df97a15dd3f954cf5
Jake Cariello <jec91@duke.edu>
2019-09-29 22:15:01 -0400

    initial DOCS for ModelWrapper

--- src/model/ModelWrapper2.java
+++ src/model/ModelWrapper2.java
@@ -7,7 +7,19 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.HashMap;
 
+/**
+ * model.ModelWrapper
+ *
+ * Eventually, call this class ModelWrapper (NOT ModelWrapper2).
+ * Master high-level class for managing a model, its metadata, and various critical operations such as creating parts
+ * and extracting potentials. This class houses the "meaty" operations of actually interacting with the model object
+ * when creating parts in the static class model.Parts.
+ *
+ * Up for consideration: where to house the meshing/solving/extracting code?
+ */
 public class ModelWrapper2 {
+
+    // INSTANCE VARIABLES
 
     // main model
     private Model model;
@@ -25,16 +37,37 @@ public class ModelWrapper2 {
     private String dest;
 
 
+    // CONSTRUCTORS
+
+    /**
+     * Default constructor (minimum of 2 arguments)
+     * @param model com.comsol.model.Model object is REQUIRED
+     * @param projectRoot the root directory of the project (might remove if unnecessary)
+     */
     ModelWrapper2(Model model, String projectRoot) {
         this.model = model;
         this.root = projectRoot;
     }
 
+    /**
+     * Overloaded constructor for passing in save directory
+     * @param model com.comsol.model.Model object is REQUIRED
+     * @param projectRoot the root directory of the project (might remove if unnecessary)
+     * @param defaultSaveDestination directory in which to save (relative to project root)
+     */
     ModelWrapper2(Model model, String projectRoot, String defaultSaveDestination) {
         this(model, projectRoot);
         this.dest = defaultSaveDestination;
     }
 
+
+    // METHODS
+
+    /**
+     *
+     * @param key type of id to get the next index for
+     * @return a string in the form of [id][N], where id is the key passed in and N is the next available index
+     */
     public String nextID(String key) {
         // default next index to 1 (assume first call of key)
         int nextIndex = 1;
@@ -45,19 +78,38 @@ public class ModelWrapper2 {
         return key + nextIndex;
     }
 
+    /**
+     * Overloaded method if saving the acquired id as a pseudonym (to be accessible later by that pseudonym)
+     * @param key the type of id to get the next index for
+     * @param pseudonym the name to give that id
+     * @return string in form [id][N] (id is key passed in, N is next available index); null (pseudonym already used)
+     */
     public String nextID(String key, String pseudonym) {
         // get next key using base method
         String id = this.nextID(key);
         // put into map as key, value pair
-        identifierPseudonyms.put(pseudonym, id);
-        return id;
+        if (! identifierPseudonyms.containsKey(pseudonym)) {
+            identifierPseudonyms.put(pseudonym, id);
+            return id; // pseudonym was NOT already in use!
+        }
+        return null; // pseudonym was already in use!
     }
 
+    /**
+     *
+     * @param psuedonym previously assigned using nextID(String, String)
+     * @return associated id (from the HashMap)
+     */
     public String getID(String psuedonym) {
         if (identifierPseudonyms.containsKey(psuedonym)) return identifierPseudonyms.get(psuedonym);
         return null;
     }
 
+    /**
+     *
+     * @param destination full path to save to
+     * @return success indicator
+     */
     public boolean saveModel(String destination) {
         try {
             this.model.save(destination);
@@ -68,14 +120,25 @@ public class ModelWrapper2 {
         }
     }
 
+    /**
+     * Convenience method for saving to relative directory (this.dest) wrt the project directory (root)
+     * @return success indicator
+     */
     public boolean saveModel() {
-        if (this.dest != null) return saveModel(this.dest);
+        if (this.dest != null) return saveModel(String.join("/", new String[]{this.root, this.dest}));
         else {
             System.out.println("Save directory not initialized");
             return false;
         }
     }
 
+    /**
+     * TODO: UNFINISHED
+     * Examples of parts: cuff, fascicle, etc.
+     * The method will automatically create required part primitives and pass the HashMap with their id's to model.Part
+     * @param name the name of the JSON configuration (same as unique indicator) for a given part
+     * @return success indicator (might remove this later)
+     */
     public boolean addPart(String name) {
 
         // extract data from json
@@ -98,5 +161,7 @@ public class ModelWrapper2 {
         return true;
 
     }
+
+
 
 }
