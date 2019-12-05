dd0c34277eb3c00daffbb555cf781009a055e16b
Eric Musselman <edm23@duke.edu>
2019-12-05 12:37:55 -0500

    adds if else for act block peri conductivity values

--- src/core/waveform.py
+++ src/core/waveform.py
@@ -82,76 +82,85 @@ class Waveform(Exceptionable, Configurable, Saveable):
 
     def rho_weerasuriya(self, f=None):
         """
-        :return: TODO
+        Calculation of perineurium impedance using results from Weerasuriya 1984 (frog). Weerasuriya discussion
+        indicates that Models A & B are better candidates than Models C & D, so I'll only consider the former pair.
+        :return: rho [ohm-m]
         """
-        w = 2*np.pi*f                          # f is in [Hz]
 
-        # Parameter values from Tables II & III
-        # [TableII, TableIII]
-        # R in ohm*cm^2
-        # C in uF/cm^2
-        r1a = np.mean([385, 493])
-        r2a = np.mean([1271, 2168])
-        c1a = np.mean([0.081, 0.075])*10**-6   # [uF/cm^2 -> F/cm^2]
-        c2a = np.mean([2.71, 5.12])*10**-6     # [uF/cm^2 -> F/cm^2]
+        # f is in [Hz]
+        if f < 10:                                  # stimulation for activation response, arbitrary cutoff of 10 Hz!
+            rho = 1149                              # [ohm-m]
 
-        r1b = np.mean([108, 155])
-        r2b = np.mean([277, 338])
-        c1b = np.mean([43.7, 202])*10**-6      # [uF/cm^2 -> F/cm^2]
-        c2b = np.mean([0.088, 0.083])*10**-6   # [uF/cm^2 -> F/cm^2]
+        else:                                       # stimulation at higher frequency for block
+            w = 2*np.pi*f
 
-        r1c = np.mean([1053, 1587])
-        r2c = np.mean([385, 493])
-        c1c = np.mean([0.106, 0.104])*10**-6   # [uF/cm^2 -> F/cm^2]
-        c2c = np.mean([2.79, 5.19])*10**-6     # [uF/cm^2 -> F/cm^2]
+            # Parameter values from Tables II & III
+            # [TableII, TableIII]
+            # R in ohm*cm^2
+            # C in uF/cm^2
+            r1a = np.mean([385, 493])
+            r2a = np.mean([1271, 2168])
+            c1a = np.mean([0.081, 0.075])*10**-6   # [uF/cm^2 -> F/cm^2]
+            c2a = np.mean([2.71, 5.12])*10**-6     # [uF/cm^2 -> F/cm^2]
 
-        r1d = np.mean([87, 102])
-        r2d = np.mean([298, 390])
-        c1d = np.mean([45.2, 201])*10**-6      # [uF/cm^2 -> F/cm^2]
-        c2d = np.mean([0.081, 0.075])*10**-6   # [uF/cm^2 -> F/cm^2]
+            r1b = np.mean([108, 155])
+            r2b = np.mean([277, 338])
+            c1b = np.mean([43.7, 202])*10**-6      # [uF/cm^2 -> F/cm^2]
+            c2b = np.mean([0.088, 0.083])*10**-6   # [uF/cm^2 -> F/cm^2]
 
-        # Model A: Z = R1 // 1/(jwC1) // [R2 + 1/jwC2]
-        za = (1/r1a + 1j*w*c1a + 1/(r2a + 1/(1j*w*c2a)))**(-1)
-        za_mag = abs(za)
-        za_mag = za_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
-        sigmas_mag = 1 / za_mag                # [S/m^2]
+            r1c = np.mean([1053, 1587])
+            r2c = np.mean([385, 493])
+            c1c = np.mean([0.106, 0.104])*10**-6   # [uF/cm^2 -> F/cm^2]
+            c2c = np.mean([2.79, 5.19])*10**-6     # [uF/cm^2 -> F/cm^2]
 
-        # Model B: Z = [R1 // 1/(jwC1)] + [R2 // 1/(jwC2)]
-        zb = (1/r1b + 1j*w*c1b)**(-1) + (1/r2b + 1j*w*c2b)**(-1)
-        zb_mag = abs(zb)
-        zb_mag = zb_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
-        sigmas_mag = 1/zb_mag                  # [S/m^2]
+            r1d = np.mean([87, 102])
+            r2d = np.mean([298, 390])
+            c1d = np.mean([45.2, 201])*10**-6      # [uF/cm^2 -> F/cm^2]
+            c2d = np.mean([0.081, 0.075])*10**-6   # [uF/cm^2 -> F/cm^2]
 
-        # Model C: Z = R2 // [ 1/(jwC2) + [R1 // 1/(jwC1)] ]
-        tmp = (1/r1c + 1j*w*c1c)**(-1)         # [R1 // 1/(jwC1)]
-        tmp = 1/(1j*w*c2c) + tmp               # Right-hand branch: [ 1/(jwC2) + [R1 // 1/(jwC1)] ]
-        zc = (1/r2c + 1/tmp)**(-1)
-        zc_mag = abs(zc)
-        zc_mag = zc_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
-        sigmas_mag = 1/zc_mag                  # [S/m^2]
+            # Model A: Z = R1 // 1/(jwC1) // [R2 + 1/jwC2]
+            za = (1/r1a + 1j*w*c1a + 1/(r2a + 1/(1j*w*c2a)))**(-1)
+            za_mag = abs(za)
+            za_mag = za_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
+            sigmas_mag = 1 / za_mag                # [S/m^2]
 
-        # Model D: Z = (1/(jwC2)) // [ R2 + [R1 // 1/(jwC1)] ]
-        tmp = (1/r1d + 1j*w*c1d)**(-1)         # [R1 // 1/(jwC1)]
-        tmp = r2d + tmp                        # Right-hand branch: [ R2 + [R1 // 1/(jwC1)] ]
-        zd = (1j*w*c2d + 1/tmp)**(-1)
-        zd_mag = abs(zd)
-        zd_mag = zd_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
-        sigmas_mag = 1/zd_mag                  # [S/m^2]
+            # Model B: Z = [R1 // 1/(jwC1)] + [R2 // 1/(jwC2)]
+            zb = (1/r1b + 1j*w*c1b)**(-1) + (1/r2b + 1j*w*c2b)**(-1)
+            zb_mag = abs(zb)
+            zb_mag = zb_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
+            sigmas_mag = 1/zb_mag                  # [S/m^2]
 
-        # Mean of models A & B
-        # Discussion indicates that models A & B are more likely candidates
-        rs21 = np.mean([za_mag, zb_mag])       # [Ohm-m^2]
+            # Model C: Z = R2 // [ 1/(jwC2) + [R1 // 1/(jwC1)] ]
+            tmp = (1/r1c + 1j*w*c1c)**(-1)         # [R1 // 1/(jwC1)]
+            tmp = 1/(1j*w*c2c) + tmp               # Right-hand branch: [ 1/(jwC2) + [R1 // 1/(jwC1)] ]
+            zc = (1/r2c + 1/tmp)**(-1)
+            zc_mag = abs(zc)
+            zc_mag = zc_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
+            sigmas_mag = 1/zc_mag                  # [S/m^2]
 
-        # Adjust for temperature - Use Q10 of 1.5, Shift to 37degC
-        temp_mammalian = 37                    # [degC]
-        temp_room = 21                         # [degC]
-        q10 = 1.5
-        rs37 = ((1/rs21)*(q10**((temp_mammalian-temp_room)/10)))**(-1)  # [Ohm-m^2]
+            # Model D: Z = (1/(jwC2)) // [ R2 + [R1 // 1/(jwC1)] ]
+            tmp = (1/r1d + 1j*w*c1d)**(-1)         # [R1 // 1/(jwC1)]
+            tmp = r2d + tmp                        # Right-hand branch: [ R2 + [R1 // 1/(jwC1)] ]
+            zd = (1j*w*c2d + 1/tmp)**(-1)
+            zd_mag = abs(zd)
+            zd_mag = zd_mag / 100**2               # [ohm-cm^2 -> ohm-m^2]
+            sigmas_mag = 1/zd_mag                  # [S/m^2]
 
-        # Convert to constant rho
-        thk_weerasuriya = 0.00002175           # [m]
-        rho = rs37/thk_weerasuriya             # [Ohm-m]
-        return rho
+            # Mean of models A & B
+            # Discussion indicates that models A & B are more likely candidates
+            rs21 = np.mean([za_mag, zb_mag])       # [ohm-m^2]
+
+            # Adjust for temperature - Use Q10 of 1.5, Shift to 37degC
+            temp_mammalian = 37                    # [degC]
+            temp_room = 21                         # [degC]
+            q10 = 1.5
+            rs37 = ((1/rs21)*(q10**((temp_mammalian-temp_room)/10)))**(-1)  # [Ohm-m^2]
+
+            # Convert to constant rho
+            thk_weerasuriya = 0.00002175           # [m]
+            rho = rs37/thk_weerasuriya             # [ohm-m]
+
+        return rho                                 # [ohm-m]
 
     def generate(self) -> List[np.ndarray]:
         """
