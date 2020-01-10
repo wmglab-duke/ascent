f4cdb1294ba55415ca965210d4175404c5b815e7
edm23 <edm23@duke.edu>
2020-01-10 11:05:17 -0500

    push

--- statistics_based_nerve.py
+++ statistics_based_nerve.py
@@ -1,17 +1,17 @@
 from copy import deepcopy
-from random import random
+import random
 
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import numpy as np
 
-d_nerve = 2000
+d_nerve = 200
 mu_fasc = 100
 std_fasc = 30
 num_fascicle = 15
 n_std_limit = 2
 min_fascicle_separation = 10
-max_iter = 1000000
+max_iter = 1000
 
 lower, upper \
 	= mu_fasc - n_std_limit * std_fasc, mu_fasc + n_std_limit * std_fasc
@@ -30,11 +30,6 @@ y_fasc = deepcopy(r)
 n_itr = deepcopy(r)
 d_fasc_skipped = deepcopy(r)
 
-print(fasc_diameters)
-print(theta)
-print(x_fasc)
-print(r)
-
 for i in range(num_fascicle):
 	while 1:
 		n_itr[i] += 1
@@ -49,7 +44,23 @@ for i in range(num_fascicle):
 		y_fasc[i] = r[i]*np.sin(theta[i])
 
 		chk = 0
-		for fasc_comp in range(0,i-1)
+		for fasc_comp in range(0, i-1):
+			tmp = np.sqrt(x_fasc[i]-x_fasc[fasc_comp])**2 + np.sqrt(y_fasc[i]-y_fasc[fasc_comp])**2
+			min_dist = min_fascicle_separation + (fasc_diameters[i] + fasc_diameters[fasc_comp])/2
+
+			if tmp < min_dist:
+				chk = 1  # too close
+				break
+
+		if chk == 0:
+			break
+
+fasc_diameters = fasc_diameters[fasc_diameters != d_fasc_skipped]
+x_fasc = x_fasc[fasc_diameters != d_fasc_skipped]
+y_fasc = y_fasc[fasc_diameters != d_fasc_skipped]
+
+
+
 
 
 # N = stats.norm(loc=mu_fasc, scale=std_fasc)
