8ed0e6efcff4284fb1c14ee96a334e85afdc5156
edm23 <edm23@duke.edu>
2020-01-11 13:21:59 -0500

    off to lib

--- statistics_based_nerve.py
+++ statistics_based_nerve.py
@@ -7,32 +7,30 @@ import numpy as np
 from shapely.geometry.point import Point
 import shapely.affinity
 
+# https://gis.stackexchange.com/questions/243459/drawing-ellipse-with-shapely
+
 d_nerve = 400
-mu_fasc = 70
+mu_fasc = 120
 std_fasc = 30
-num_fascicle = 10
+num_fascicle = 1
 n_std_limit = 2
 min_fascicle_separation = 10
 max_iter = 1000
 
-lower, upper \
-    = mu_fasc - n_std_limit * std_fasc, mu_fasc + n_std_limit * std_fasc
+lower, upper = mu_fasc - n_std_limit * std_fasc, mu_fasc + n_std_limit * std_fasc
 
 X = stats.truncnorm((lower - mu_fasc) / std_fasc,
                     (upper - mu_fasc) / std_fasc,
                     loc=mu_fasc,
                     scale=std_fasc)
 
-fasc_diameters = np.sort(X.rvs(num_fascicle))
-
+fasc_diameters = np.sort(X.rvs(num_fascicle))[::-1]
 r = np.zeros(np.size(fasc_diameters))
-theta = deepcopy(r)
-x_fasc = deepcopy(r)
-y_fasc = deepcopy(r)
-n_itr = deepcopy(r)
 d_fasc_skipped = deepcopy(r)
 
-fascicle = []
+fascicles = []
+n_itr = []
+nerve = shapely.geometry.Point(0, 0).buffer(d_nerve / 2)
 
 
 def get_random_point_in_polygon(poly):
@@ -43,61 +41,59 @@ def get_random_point_in_polygon(poly):
             return p
 
 
-# ellipse = ((0, 0), (d_nerve/2, d_nerve/2), 0)
-nerve = shapely.geometry.Point(0, 0).buffer(d_nerve/2)
-
 for i in range(num_fascicle):
-    while 1:
+    n_itr.append(0)
+
+    while True:
         n_itr[i] += 1
+        print(n_itr[i])
 
         if n_itr[i] > max_iter:
             d_fasc_skipped[i] = fasc_diameters[i]
             break
 
-        # https://gis.stackexchange.com/questions/243459/drawing-ellipse-with-shapely
-
-        # 1st elem = center point (x,y) coordinates
-        # 2nd elem = the two semi-axis values (along x, along y)
-        # 3rd elem = angle in degrees between x-axis of the Cartesian base
-        #            and the corresponding semi-axis
-
         p = get_random_point_in_polygon(nerve)
-
-        fascicle.append(shapely.geometry.Point(p).buffer(1))
-        shapely.affinity.scale(fascicle[i], int(fasc_diameters[i]), int(fasc_diameters[i]))
+        fascicle_attempt = shapely.geometry.Point(p).buffer(fasc_diameters[i]/2)
 
         chk = 0
+
+        if fascicle_attempt.intersects(nerve):
+            print('nerve intersects')
+            chk = 1
+            break
+
         for fasc_comp in range(0, i - 1):
-            if fascicle[fasc_comp].intersects(shapely.affinity.scale(fascicle[i],
-                                                                     int(min_fascicle_separation),
-                                                                     int(min_fascicle_separation))):
-                print("intersects")
+            print('fascicle intersects')
+
+            if fascicles[fasc_comp].intersects(fascicle_attempt):
+                print('intersects')
                 chk = 1
                 break
 
-            if chk == 0:
-                break
+        if chk == 0:
+            print('appending')
+            fascicles.append(fascicle_attempt)
+            break
 
-fasc_diameters_post = [f for f, d in zip(fasc_diameters, d_fasc_skipped) if abs(f - d) < 0.001]
-x_fasc = [x for x, f, d in zip(x_fasc, fasc_diameters, d_fasc_skipped) if abs(f - d) < 0.001]
-y_fasc = [y for y, f, d in zip(y_fasc, fasc_diameters, d_fasc_skipped) if abs(f - d) < 0.001]
+fasc_diameters_post = [f for f, d in zip(fasc_diameters, d_fasc_skipped) if abs(f - d) > 0.001]
 
-print(fasc_diameters_post)
 fig, ax = plt.subplots()
 # Plot Nerve
-nerve = plt.Circle((0, 0), d_nerve / 2, color='b')
-ax.add_artist(nerve)
-plt.plot(x_fasc, y_fasc, 'ro')
-plt.plot(x_fasc, y_fasc, 'ro')
+# nerve = plt.Circle((0, 0), d_nerve / 2, color='b')
+# plt.plot(x_fasc, y_fasc, 'ro')
+# plt.plot(x_fasc, y_fasc, 'ro')
 
-for fasc in range(len(fasc_diameters_post)):
-    print(fasc)
-    fascPlot = plt.Circle((x_fasc[fasc], y_fasc[fasc]), fasc_diameters_post[fasc] / 2, color='r')
-    ax.add_artist(fascPlot)
+x, y = nerve.exterior.xy
+plt.plot(x, y)
+
+for fascicle in fascicles:
+    x, y = fascicle.exterior.xy
+    plt.plot(x, y)
 
 ax.set_aspect('equal', 'box')
 plt.ylim(ymax=d_nerve / 2, ymin=-d_nerve / 2)
 plt.xlim(xmax=d_nerve / 2, xmin=-d_nerve / 2)
+
 plt.show()
 
 #
