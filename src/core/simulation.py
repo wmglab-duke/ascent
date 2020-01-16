64d0f32978309b20f6dacbc1b2dbbfbf7c494693
Jake Cariello <jec91@duke.edu>
2020-01-16 11:12:24 -0500

    skeleton

---
+++ src/core/simulation.py
@@ -0,0 +1,11 @@
+from core import Sample
+from utils import Exceptionable, Configurable, Saveable, SetupMode
+
+
+class Simulation(Exceptionable, Configurable, Saveable):
+
+    def __init__(self, sample: Sample, exception_config: list):
+
+        # Initializes superclasses
+        Exceptionable.__init__(self, SetupMode.OLD, exception_config)
+        Configurable.__init__(self)
