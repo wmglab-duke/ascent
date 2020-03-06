6640a0140edaea630607423da0ae4996b270d01b
edm23 <edm23@duke.edu>
2020-03-06 15:47:03 -0500

    skeleton for query class

---
+++ query.py
@@ -0,0 +1,55 @@
+from typing import Union
+
+from core import Sample
+from src.utils import *
+
+
+class Query(Exceptionable, Configurable, Saveable):
+    """
+    """
+
+    def __init__(self, exceptions_config: list):
+        """
+
+        :param exceptions_config:
+        """
+
+        # set up superclasses
+        Configurable.__init__(self)
+        Exceptionable.__init__(self, SetupMode.OLD, exceptions_config)
+
+        # initialize
+
+    def set_query(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def run(self):
+        """
+
+        :return:
+        """
+
+    def summary(self) -> dict:
+        """
+
+        :return:
+        """
+
+    def fetch_config(self) -> dict:
+        """
+
+        :return:
+        """
+
+    def fetch_object(self) -> Union[Sample, Sim]:
+        """
+
+        :return:
+        """
+
+
+
