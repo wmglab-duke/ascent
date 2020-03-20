9628a691a1a9a9f920deeae9c8d01879ae738dbd
Jake Cariello <jec91@duke.edu>
2020-03-20 13:51:43 -0400

    finished body of Query.run() - now need to write helper method Query._match()

--- src/core/query.py
+++ src/core/query.py
@@ -20,6 +20,7 @@ class Query(Exceptionable, Configurable, Saveable):
 
         self._ran: bool = False  # marker will be set to True one self.run() is called (as is successful)
         self.add(SetupMode.NEW, Config.CRITERIA, criteria_path)  # initialize criteria
+        self._result = None  # begin with empty result
 
     def run(self):
         """
@@ -27,34 +28,107 @@ class Query(Exceptionable, Configurable, Saveable):
         :return: result as a dict
         """
 
+        # initialize empty result
+        result = {}
+
         # preliminarily find sample, model, and sim filter indices if applicable (else None)
         sample_indices = self.search(Config.CRITERIA, 'indices', 'sample')
         model_indices = self.search(Config.CRITERIA, 'indices', 'model')
         sim_indices = self.search(Config.CRITERIA, 'indices', 'sim')
 
+        # criteria for each layer
         sample_criteria = self.search(Config.CRITERIA, 'sample')
         model_criteria = self.search(Config.CRITERIA, 'model')
         sim_criteria = self.search(Config.CRITERIA, 'sim')
 
-        samples_dir = 'samples'
+        # control if missing sim criteria or both sim and model criteria
+        include_downstream = self.search(Config.CRITERIA, 'include_downstream')
+
+        # labeling for samples level
+        samples_key = 'samples'
+        samples_dir = samples_key
+
+        # init list of samples in result
+        result[samples_key] = []
+
+        # loop samples
         for sample in os.listdir(samples_dir):
             # skip this sample if applicable
             if sample_indices is not None and int(sample) not in sample_indices:
                 continue
 
-            # FILTER SAMPLE LEVEL HERE
+            # if applicable, check against sample criteria
+            if sample_criteria is not None:
+                if not self._match(
+                    sample_criteria,
+                    self.load(os.path.join(samples_dir, sample, 'sample.json'))
+                ):
+                    continue
 
-            models_dir = os.path.join(samples_dir, sample, 'models')
+            # labeling for models level
+            models_key = 'models'
+            models_dir = os.path.join(samples_dir, sample, models_key)
+
+            # post-filtering, add empty SAMPLE to result
+            # important to remember that this is at END of list
+            result[samples_key].append({
+                'index': int(sample),
+                models_key: []
+            })
+
+            # if no downstream criteria and NOT including downstream, skip lower loops
+            # note also that the post loop removal of samples will be skipped (as we desire in this case)
+            if model_criteria is None and sim_criteria is None and not include_downstream:
+                continue
+
+            # loop models
             for model in os.listdir(models_dir):
+                # if there are filter indices for models, use them
                 if model_indices is not None and int(model) not in model_indices:
                     continue
 
-                # FILTER MODEL LEVEL HERE
+                # if applicable, check against model criteria
+                if model_criteria is not None:
+                    if not self._match(
+                        model_criteria,
+                        self.load(os.path.join(models_dir, model, 'model.json'))
+                    ):
+                        continue
 
-                sims_dir = os.path.join(models_dir, model, 'sims')
+                # labeling for sims level
+                sims_key = 'sims'
+                sims_dir = os.path.join(models_dir, model, sims_key)
+
+                # post-filtering, add empty MODEL to result
+                # important to remember that this is at END of list
+                result[samples_key][-1][models_key].append({
+                    'index': int(model),
+                    sims_key: []
+                })
+
+                # if no downstream criteria and NOT including downstream, skip lower loops
+                # note also that the post loop removal of models will be skipped (as we desire in this case)
+                if sim_criteria is None and not include_downstream:
+                    continue
+
+                # loop sims
                 for sim in os.listdir(sims_dir):
                     if sim_indices is not None and int(sim) not in sim_indices:
                         continue
+
+                    # if applicable, check against model criteria
+                    if sim_criteria is not None:
+                        if not self._match(
+                            sim_criteria,
+                            self.load(os.path.join('config', 'user', 'sims', sim + '.json'))
+                        ):
+                            continue
+
+                    # post-filtering, add SIM to result
+                    result[samples_key][-1][models_key][-1][sims_key].append(int(sim))
+
+        self._result = result
+
 
 
 
@@ -64,6 +138,8 @@ class Query(Exceptionable, Configurable, Saveable):
 
         :return:
         """
+        if self._result is None:
+            self.throw(53)
 
     def fetch_config(self) -> dict:
         """
@@ -76,6 +152,16 @@ class Query(Exceptionable, Configurable, Saveable):
 
         :return:
         """
+
+    def _match(self, criteria: dict, data: dict) -> bool:
+        """
+
+        :param criteria:
+        :param data:
+        :return:
+        """
+
+        return True
 
 
 
