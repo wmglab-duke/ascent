9962ae9e25707515dcc44749bb979cb85137eb6b
Jake Cariello <jec91@duke.edu>
2020-03-20 18:28:34 -0400

    minor changes to query_test.py

--- src/tests/query_test.py
+++ src/tests/query_test.py
@@ -2,7 +2,9 @@ import os
 from src.core.query import Query
 from src.utils import Config, Object
 
+# THIS LINE IS USER-SPECIFIC UNTIL THERE IS A FIXED PLACE FOR ANALYSIS SCRIPTS
 project_root = '/Users/jakecariello/Box/Documents/Pipeline/access'
+
 query_criteria = os.path.join(project_root, 'config/user/query_criteria/0.json')
 
 # NECESSARY PREREQUISITE FOR USING QUERY
@@ -12,4 +14,5 @@ q = Query(query_criteria)
 
 q.run()
 
-s = q.summary()
+summary = q.summary()
+
