09ccfdc817eb33933d1f3bdcaaa0045bb77f4318
Jake Cariello <jake.cariello@duke.edu>
2020-04-25 12:55:47 -0400

    fix manual rotation

--- src/runner.py
+++ src/runner.py
@@ -408,6 +408,7 @@ class Runner(Exceptionable, Configurable):
 
             if cuff_rotation_mode == CuffRotationMode.MANUAL:
                 theta_f = theta_i
+                theta_c = self.search(Config.MODEL, 'cuff', 'rotate', 'pos_ang') * 2 * np.pi / 360
             else:  # cuff_rotation_mode == CuffRotationMode.AUTOMATIC
                 if r_i < r_f:
                     theta_f = (r_f / r_i - 1) * theta_i
