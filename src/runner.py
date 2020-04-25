9223ea8c794f2b20b684bfecb9bf6d47e42c81be
Jake Cariello <jake.cariello@duke.edu>
2020-04-25 14:10:46 -0400

    minor

--- src/runner.py
+++ src/runner.py
@@ -365,7 +365,7 @@ class Runner(Exceptionable, Configurable):
         if not slide.monofasc():
             reference_x, reference_y = slide.fascicle_centroid()
         theta_c = np.arctan2(y - reference_y, x - reference_x)
-        # theta_c += self.search(Config.MODEL, 'cuff', 'rotate', 'add_ang') * 2 * np.pi / 360
+        theta_f = self.search(Config.MODEL, 'cuff', 'rotate', 'add_ang') * 2 * np.pi / 360
 
         # calculate final necessary radius by adding buffer
         r_f = r_bound + cuff_r_buffer
@@ -394,7 +394,7 @@ class Runner(Exceptionable, Configurable):
             if not r_f <= r_i:
                 self.throw(51)
 
-            theta_f = 0
+            theta_f += 0
         else:
             # get initial cuff radius
             r_i_str: str = [item["expression"] for item in cuff_config["params"]
@@ -412,9 +412,9 @@ class Runner(Exceptionable, Configurable):
             #
             # else:  # cuff_rotation_mode == CuffRotationMode.AUTOMATIC
             if r_i < r_f:
-                theta_f = (r_f / r_i - 1) * theta_i
+                theta_f += (r_f / r_i - 1) * theta_i
             else:
-                theta_f = 0
+                theta_f += 0
 
         #
         offset = 0
