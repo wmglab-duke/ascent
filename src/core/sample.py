9e59126bc07db1722d9338d4fd9bddebdfedaaac
Jake Cariello <jake.cariello@duke.edu>
2020-04-30 20:46:30 -0400

    minor console output changes

--- src/core/sample.py
+++ src/core/sample.py
@@ -320,11 +320,13 @@ class Sample(Exceptionable, Configurable, Saveable):
                 dist = self.search(Config.SAMPLE, "min_fascicle_separation", "dist")
                 nerve_add = None
 
+                print('\t\tensuring minimum fascicle separation of {} um'.format(dist))
+
                 if 'nerve_addition' in self.search(Config.SAMPLE, 'min_fascicle_separation').keys():
                     nerve_add = self.search(Config.SAMPLE, 'min_fascicle_separation', 'nerve_addition')
-                    print('\t\taccounting for additional nerve boundary buffer: {}'.format(nerve_add))
+                    print('\t\taccounting for additional nerve boundary buffer: {} um'.format(nerve_add))
 
-                print('\t\tensuring minimum fascicle separation of {} um'.format(dist))
+
 
                 deformable = Deformable.from_slide(slide,
                                                    ReshapeNerveMode.CIRCLE,
