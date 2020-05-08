70d17c090e7e5ff25effe604e47ec863e3f00d9b
Jake Cariello <jake.cariello@duke.edu>
2020-05-08 17:48:00 -0400

    sectionwise output utility: fiber_to_secitonwise.py

---
+++ examples/analysis/fiber_to_sectionwise.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.7
+
+import sys
+import os
+
+import numpy as np
+
+usage: str = 'USAGE: ./fiber_to_sectionwise <source_path.dat> <target_path.txt>\n' \
+             '(must type ".dat" and ".txt" -- these are convention)'
+
+if len(sys.argv) != 3:
+    print('Invalid argument count: {}\n{}'.format(len(sys.argv), usage))
+    exit()
+
+source: str = sys.argv[2]
+target: str = sys.argv[3]
+target_dir, _ = os.path.split(target)
+
+for name, value in zip(['source', 'target directory'], [source, target_dir]):
+    if not os.path.exists(value):
+        print('Invalid path for {}: {}\n{}'.format(name, value, usage))
+
+data: np.ndarray = np.loadtxt(source)[1:]
+
+with open(target, 'w+') as handle:
+    # write coordinates
+    handle.write('%% Coordinates\n')
+    for i in range(len(data)):
+        handle.write('{}\t{}\n'.format(data[i, 0], data[i, 1]))
+
+print('Sectionwise2d file written to: {}'.format(target))
