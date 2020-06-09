b2d8251098df4ad19221e28bd46947eb2b61128f
Jake Cariello <jake.cariello@duke.edu>
2020-06-08 23:19:34 -0400

    streamline validation assertions in batch.py

--- src/neuron/batch.py
+++ src/neuron/batch.py
@@ -6,7 +6,7 @@ import re
 import sys
 import time
 
-# os.system('module load Neuron/7.6.2')
+ALLOWED_SUBMISSION_CONTEXTS = ['cluster', 'local']
 
 if not os.path.exists(os.path.join('MOD_Files/x86_64')):
     os.chdir(os.path.join('MOD_Files'))
@@ -14,94 +14,95 @@ if not os.path.exists(os.path.join('MOD_Files/x86_64')):
     os.chdir('..')
 
 for run_number in sys.argv[1:]:
-    # if not run_number.isnumeric():
-    #     raise Exception(
-    #         'Invalid argument: {}\n'
-    #         'All arguments must be positive integers.'.format(run_number)
-    #     )
+    assert re.search('[0-9]+', run_number), 'Encountered non-number run number argument: {}'.format(run_number)
 
     filename = os.path.join('runs', run_number + '.json')
 
-    if not os.path.exists(filename):
-        raise Exception('Invalid run number: {}'.format(run_number))
+    assert os.path.exists(filename), 'Run configuration not found: {}'.format(run_number)
 
+    run = {}
     with open(filename, 'r') as file:
-        run = json.load(file)
+        run: dict = json.load(file)
 
-        sample = run.get('sample')
-        models = run.get('models')
-        sims = run.get('sims')
+    assert len(run.items) > 0, 'Encountered empty run configuration: {}'.format(filename)
 
-        for model in models:
-            for sim in sims:
-                sim_dir = os.path.join('n_sims')
-                sim_name_base = '{}_{}_{}_'.format(sample, model, sim)
+    sample = run.get('sample', [])
+    models = run.get('models', [])
+    sims = run.get('sims', [])
+    submission_context = run.get('submission_context', 'cluster')
 
-                for sim_name in [x for x in os.listdir(sim_dir) if sim_name_base in x]:
-                    sim_path = os.path.join(sim_dir, sim_name)
-                    fibers_path = os.path.join(sim_path, 'data', 'inputs')
-                    output_path = os.path.join(sim_path, 'data', 'outputs')
-                    out_dir = os.path.abspath(os.path.join(sim_path, 'logs', 'out'))
-                    err_dir = os.path.abspath(os.path.join(sim_path, 'logs', 'err'))
+    assert submission_context in ALLOWED_SUBMISSION_CONTEXTS, 'Invalid submission context: {}'.format(submission_context)
 
-                    # ensure log directories exist
-                    for cur_dir in [out_dir, err_dir]:
-                        if not os.path.exists(cur_dir):
-                            os.makedirs(cur_dir)
+    for model in models:
+        for sim in sims:
+            sim_dir = os.path.join('n_sims')
+            sim_name_base = '{}_{}_{}_'.format(sample, model, sim)
 
-                    # ensure blank.hoc exists
-                    blank_path = os.path.join(sim_path, 'blank.hoc')
-                    if not os.path.exists(blank_path):
-                        open(blank_path, 'w').close()
+            for sim_name in [x for x in os.listdir(sim_dir) if sim_name_base in x]:
+                sim_path = os.path.join(sim_dir, sim_name)
+                fibers_path = os.path.join(sim_path, 'data', 'inputs')
+                output_path = os.path.join(sim_path, 'data', 'outputs')
+                out_dir = os.path.abspath(os.path.join(sim_path, 'logs', 'out'))
+                err_dir = os.path.abspath(os.path.join(sim_path, 'logs', 'err'))
 
-                    print('\n\n################ {} ################\n\n'.format(sim_name))
+                # ensure log directories exist
+                for cur_dir in [out_dir, err_dir]:
+                    if not os.path.exists(cur_dir):
+                        os.makedirs(cur_dir)
 
-                    for fiber_filename in [x for x in os.listdir(fibers_path)
-                                           if re.match('inner[0-9]+_fiber[0-9]+\\.dat', x)]:
-                        master_fiber_name = str(fiber_filename.split('.')[0])
-                        inner_name, fiber_name = tuple(master_fiber_name.split('_'))
-                        inner_ind = int(inner_name.split('inner')[-1])
-                        fiber_ind = int(fiber_name.split('fiber')[-1])
+                # ensure blank.hoc exists
+                blank_path = os.path.join(sim_path, 'blank.hoc')
+                if not os.path.exists(blank_path):
+                    open(blank_path, 'w').close()
 
-                        thresh_path = os.path.join(output_path, 'thresh_inner{}_fiber{}.dat'.format(inner_ind, fiber_ind))
-                        if os.path.exists(thresh_path):
-                            print('Found {} -->\t\tskipping inner ({}) fiber ({})'.format(thresh_path, inner_ind, fiber_ind))
-                            continue
+                print('\n\n################ {} ################\n\n'.format(sim_name))
 
-                        # write start.slurm
-                        start_path = os.path.join(sim_path, 'start.slurm')
-                        if os.path.exists(start_path):
-                            raise Exception('start.slurm already exists (not expected) check path/implementation')
+                for fiber_filename in [x for x in os.listdir(fibers_path)
+                                        if re.match('inner[0-9]+_fiber[0-9]+\\.dat', x)]:
+                    master_fiber_name = str(fiber_filename.split('.')[0])
+                    inner_name, fiber_name = tuple(master_fiber_name.split('_'))
+                    inner_ind = int(inner_name.split('inner')[-1])
+                    fiber_ind = int(fiber_name.split('fiber')[-1])
 
-                        stimamp_top = -10
-                        stimamp_bottom = -0.01
+                    thresh_path = os.path.join(output_path, 'thresh_inner{}_fiber{}.dat'.format(inner_ind, fiber_ind))
+                    if os.path.exists(thresh_path):
+                        print('Found {} -->\t\tskipping inner ({}) fiber ({})'.format(thresh_path, inner_ind, fiber_ind))
+                        continue
 
-                        with open(start_path, 'w') as handle:
-                            lines = [
-                                '#!/bin/bash\n',
-                                'cd {}\n'.format(sim_path),
-                                'cp -p ../../MOD_Files/x86_64/special .\n',
-                                'chmod a+rwx special\n',
-                                './special -nobanner blank.hoc '
-                                '-c \"strdef sim_path\" '
-                                '-c \"sim_path=\\\"{}\\\"\" '
-                                '-c \"inner_ind={}\" '
-                                '-c \"fiber_ind={}\" '
-                                '-c \"stimamp_top={}\" '
-                                '-c \"stimamp_bottom={}\" '
-                                '-c \"load_file(\\\"launch.hoc\\\")\"\n'.format(sim_path,
-                                                                                inner_ind,
-                                                                                fiber_ind,
-                                                                                stimamp_top,
-                                                                                stimamp_bottom)
-                            ]
-                            handle.writelines(lines)
+                    # write start.slurm
+                    start_path = os.path.join(sim_path, 'start.slurm')
+                    if os.path.exists(start_path):
+                        raise Exception('start.slurm already exists (not expected) check path/implementation')
 
-                        # submit batch job for fiber
-                        job_name = '{}_{}'.format(sim_name, master_fiber_name)
-                        print('\n{}'.format(job_name))
+                    stimamp_top = -10
+                    stimamp_bottom = -0.01
 
-                        # TODO this will change for local running
+                    with open(start_path, 'w') as handle:
+                        lines = [
+                            '#!/bin/bash\n',
+                            'cd {}\n'.format(sim_path),
+                            'cp -p ../../MOD_Files/x86_64/special .\n',
+                            'chmod a+rwx special\n',
+                            './special -nobanner blank.hoc '
+                            '-c \"strdef sim_path\" '
+                            '-c \"sim_path=\\\"{}\\\"\" '
+                            '-c \"inner_ind={}\" '
+                            '-c \"fiber_ind={}\" '
+                            '-c \"stimamp_top={}\" '
+                            '-c \"stimamp_bottom={}\" '
+                            '-c \"load_file(\\\"launch.hoc\\\")\"\n'.format(sim_path,
+                                                                            inner_ind,
+                                                                            fiber_ind,
+                                                                            stimamp_top,
+                                                                            stimamp_bottom)
+                        ]
+                        handle.writelines(lines)
+
+                    # submit batch job for fiber
+                    job_name = '{}_{}'.format(sim_name, master_fiber_name)
+                    print('\n{}'.format(job_name))
+
+                    if submission_context == 'cluster':
                         command = ' '.join([ 
                             'sbatch',
                             '--job-name={}'.format(job_name),
@@ -119,3 +120,6 @@ for run_number in sys.argv[1:]:
 
                         # remove start.slurm
                         os.remove(start_path)
+                    
+                    elif submission_context == 'local':
+                        pass
