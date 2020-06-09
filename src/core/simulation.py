88d5c6484f3519756cd9e809bae4ffced8f5ee32
Jake Cariello <jake.cariello@duke.edu>
2020-06-08 22:06:09 -0400

    add export_neuron_files method

--- src/core/simulation.py
+++ src/core/simulation.py
@@ -13,7 +13,7 @@ from .hocwriter import HocWriter
 from .fiberset import FiberSet
 from .waveform import Waveform
 from src.core import Sample
-from src.utils import Exceptionable, Configurable, Saveable, SetupMode, Config, WriteMode, FiberXYMode
+from src.utils import Exceptionable, Configurable, Saveable, SetupMode, Config, WriteMode, FiberXYMode, Env
 
 
 class Simulation(Exceptionable, Configurable, Saveable):
@@ -388,6 +388,11 @@ class Simulation(Exceptionable, Configurable, Saveable):
                 os.path.join(sim_dir, product_index),
                 sim_export_base + product_index
             )
+
+    @staticmethod
+    def export_neuron_files(target: str):
+        neuron_dir = os.path.join(os.environ[Env.PROJECT_PATH.value], 'src', 'neuron')
+        shutil.copytree(neuron_dir, target)
 
     @staticmethod
     def import_n_sims(sample: int, model: int, sim: int, sim_dir: str, source: str):
