7ee3840760dfc9d6b8005f7377eb77fd43f3633d
edm23 <edm23@duke.edu>
2020-06-24 15:48:43 -0400

    prevents extrapolation outside of MRG fiber diamters for MRG_INTERPOLATION - throws error

--- src/core/fiberset.py
+++ src/core/fiberset.py
@@ -451,6 +451,8 @@ class FiberSet(Exceptionable, Configurable, Saveable):
                         inter_length = eval(inter_length_str)
                         delta_z = eval(delta_z_str)
                     elif fiber_geometry_mode_name == FiberGeometry.MRG_INTERPOLATION.value:
+                        if diameter > 16.0:
+                            self.throw(77)
                         if diameter >= 5.26:
                             delta_z = eval(delta_z_str["diameter_greater_or_equal_5.26um"])
                         else:
