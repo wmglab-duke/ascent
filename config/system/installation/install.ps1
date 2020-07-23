ae5671e45b2ae5e0b410587fa976bfcdcdbc2532
Jake Cariello <jake.cariello@duke.edu>
2020-07-23 16:01:20 -0400

    again

--- config/system/installation/install.ps1
+++ config/system/installation/install.ps1
@@ -9,8 +9,8 @@ if ($decision -eq 0) {
     # package installation
     conda create --name ascent -y
     conda activate ascent
-    conda install -y python=3.7
-    pip install pip==20.1.1 pillow==5.2.0 numpy==1.16.4 shapely==1.6.4.post2 matplotlib==3.2.1 pyclipper==1.1.0.post3 pygame==1.9.6 libtiff==4.0 pymunk==5.5.0 scipy==1.1.0 pandas==0.25.1 openpyxl==3.0.3 opencv-python quantiphy
+    pip3 install --upgrade pip setuptools wheel
+    pip3 install pillow==5.2.0 numpy==1.16.4 shapely==1.6.4.post2 matplotlib==3.2.1 pyclipper==1.1.0.post3 pygame==1.9.6 libtiff==4.0 pymunk==5.5.0 scipy==1.1.0 pandas==0.25.1 openpyxl==3.0.3 opencv-python quantiphy
     
     # shortcut creation
     $title    = 'ASCENT Conda Environment'
