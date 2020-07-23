af442acf960ae1d3cfd72be13c10cc9d5fc7c290
Jake Cariello <jake.cariello@duke.edu>
2020-07-23 16:43:40 -0400

    again

--- config/system/installation/install.ps1
+++ config/system/installation/install.ps1
@@ -9,9 +9,10 @@ if ($decision -eq 0) {
     # package installation
     conda create --name ascent -y
     conda activate ascent
+    conda uninstall -y python
     conda install -y python=3.7
     python C:\Users\jec91\Miniconda3\envs\ascent\Lib\site-packages\pip install --upgrade pip setuptools wheel
-    python C:\Users\jec91\Miniconda3\envs\ascent\Lib\site-packages\pip install pillow>=5.2.0 numpy>=1.16.4 shapely>=1.6.4 matplotlib>=3.2.1 pyclipper>=1.1.0 pygame>=1.9.6 libtiff==0.4.0 pymunk>=5.5.0 scipy>=1.1.0 pandas>=0.25.1 openpyxl>=3.0.3 opencv-python quantiphy
+    python C:\Users\jec91\Miniconda3\envs\ascent\Lib\site-packages\pip install pillow>=5.2.0 numpy>=1.16.4 shapely>=1.6.4 matplotlib>=3.2.1 pyclipper>=1.1.0 pygame>=1.9.6 pymunk>=5.5.0 scipy>=1.1.0 pandas>=0.25.1 openpyxl>=3.0.3 opencv-python quantiphy
     
     # shortcut creation
     $title    = 'ASCENT Conda Environment'
