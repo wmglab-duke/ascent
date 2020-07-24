b44e393dda12653edff8e0db8fd5f67db2299c41
Jake Cariello <jake.cariello@duke.edu>
2020-07-24 12:19:46 -0400

    comment out __init__

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,5 +1,5 @@
-from .env_setup import run
-from .import_n_sims import run
-from .mock_morphology_generator import run
-from .pipeline import run
-from .install import run
+# from .env_setup import run
+# from .import_n_sims import run
+# from .mock_morphology_generator import run
+# from .pipeline import run
+# from .install import run
