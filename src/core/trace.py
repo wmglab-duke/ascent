1cd2b292b428a57476c56ed02a7647752e4e50bf
Jake Cariello <jake.cariello@duke.edu>
2020-07-24 11:52:45 -0400

    fix typo in Trace

--- src/core/trace.py
+++ src/core/trace.py
@@ -618,7 +618,7 @@ class Trace(Exceptionable):
         return c is not None and np.math.hypot(p[0] - c[0], p[1] - c[1]) <= c[2] * _MULTIPLICATIVE_EPSILON
 
     # Returns twice the signed area of the triangle defined by (x0, y0), (x1, y1), (x2, y2).
-    def _cross_product(x0, y0, x1, y1, x2, y2):
+    def _cross_product(self, x0, y0, x1, y1, x2, y2):
         return (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0)
 
     def smallest_enclosing_circle_naive(self) -> Tuple[float, float, float]:
