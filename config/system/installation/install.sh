fe0a8cda433e1c4a71adc44fc94065640b82be95
Jake Cariello <jake.cariello@duke.edu>
2020-07-25 15:29:43 -0400

    installation now downloads json tools JAR (removed from repo)

--- config/system/installation/install.sh
+++ config/system/installation/install.sh
@@ -1,23 +1,14 @@
 #! /bin/bash
 
-# confirm location
-while true; do
-    read -p "Have you navigated to the root of the ASCENT repository? [y/N] " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) echo "Please do so and re-run."; exit 1;;
-        * ) echo "Please do so and re-run."; exit 1;;
-    esac
-done
-
 # package installation
 source ~/opt/miniconda3/etc/profile.d/conda.sh
 conda create -n ascent -y
 conda activate ascent
 conda install -y python=3.7
-python ~/opt/miniconda3/envs/ascent/bin/pip install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel
 pip install pillow>=5.2.0 numpy>=1.16.4 shapely>=1.6.4 matplotlib>=3.2.1 pyclipper>=1.1.0 pygame>=1.9.6 pymunk>=5.5.0 scipy>=1.1.0 pandas>=0.25.1 openpyxl>=3.0.3 opencv-python quantiphy
 conda install -y shapely
+rm =*  # weird bug where pip creates empty files named "=<version>"
 
 echo
 echo "Installation complete."
