d4504daf100d7f4cda591714520dfd86e324eb13
Jake Cariello <jakecariello@gmail.com>
2020-11-11 17:49:17 -0500

    client params

--- examples/analysis/ap_time_and_loc.py
+++ examples/analysis/ap_time_and_loc.py
@@ -22,9 +22,9 @@ q = Query({
 }).run()
 
 q.ap_time_and_location(
-    delta_V=-30,
+    delta_V=60,
     plot=True,
-    absolute_voltage=True,
+    absolute_voltage=False,
     n_sim_label_override='10 µm MRG Fiber',
     model_labels=[
         '5000 µm model radius',
