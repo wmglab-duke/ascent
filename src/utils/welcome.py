4029e6773f981d0151f8f09cd3fb229cc7465678
Entirity <entirity001@gmail.com>
2021-09-07 08:55:11 -0400

    added welcome message to ascent

---
+++ src/utils/welcome.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Welcome message for ascent software
+"""
+def welcome():
+    print("""
+Funding for development of this sofware provided by NIH SPARC
+                                                                                      
+   .@@@@@@@       (@@@@@@@#          %%%           (@@@@@@%.        ,&@@@@%     
+   @@#   %@@      (@@.   @@%        *%%%(          (@@   *@@@      @@@.  #@@*   
+   @@@   (&&      (@@.   &@@        %%%%%          (@@    &@@      @@#   .@@(   
+    ,@@@(         (@@.   @@&       ,%%%%%*         (@@    @@@      @@#          
+       #@@@.      (@@@@@@@&        #%% %%%         (@@@@@@@&       @@#          
+   @@/   /@@      (@@.             %%/ (%%,        (@@   @@(       @@#   .&&(   
+   @@@   @@@      (@@.            %%%   %%%        (@@   /@@,      @@@   ,@@/   
+    .%@@@&,       (@@.           .%%*   (%%,       (@@    %@@.      %@@@@@@.    
+                                 #%%     %%#                                    
+   #####%%%%%%%%%%%%%%%%%%%%%%%%%%%*     /%%    ###############((((((((((((/,   
+                                          %%#  ##*                              
+                                          *%%*##,                               
+                                           %%%#.                                
+                                           *%%                                  
+                                                                               
+                                           """)
+                                               
+    print("""
+The copyrights of this software are owned by Duke University.
+Please refer to the LICENSE.txt and README.txt files for licensing instructions.
+The source code can be found on the following GitHub repository: https://github.com/wmglab-duke/ascent
+---
+""")
