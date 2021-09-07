79761afd6765357c3e231f0fdfa46f190cfe4b87
Entirity <entirity001@gmail.com>
2021-09-07 09:36:09 -0400

    shipping

--- src/utils/welcome.py
+++ src/utils/welcome.py
@@ -19,8 +19,8 @@ Funding for development of this sofware provided by NIH SPARC OT2 OD025340\n
                            .                     """)
                                                
     print("""
-The copyrights of this software are owned by Duke University
-Please refer to the LICENSE.txt and README.txt files for licensing instructions\n
+The copyrights of this software are owned by Duke University.
+Please refer to the LICENSE.txt and README.txt files for licensing instructions.\n
 The source code can be found at the following GitHub repository: \nhttps://github.com/wmglab-duke/ascent
 ---
 """)
