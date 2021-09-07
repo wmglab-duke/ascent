07db2a22edfa4754eb749f8e8c1870dc0ad809fe
Entirity <entirity001@gmail.com>
2021-09-07 09:16:07 -0400

    a little change to message

--- src/utils/welcome.py
+++ src/utils/welcome.py
@@ -5,28 +5,22 @@ Welcome message for ascent software
 """
 def welcome():
     print("""
-Funding for development of this sofware provided by NIH SPARC
-                                                                                      
-   .@@@@@@@       (@@@@@@@#          %%%           (@@@@@@%.        ,&@@@@%     
-   @@#   %@@      (@@.   @@%        *%%%(          (@@   *@@@      @@@.  #@@*   
-   @@@   (&&      (@@.   &@@        %%%%%          (@@    &@@      @@#   .@@(   
-    ,@@@(         (@@.   @@&       ,%%%%%*         (@@    @@@      @@#          
-       #@@@.      (@@@@@@@&        #%% %%%         (@@@@@@@&       @@#          
-   @@/   /@@      (@@.             %%/ (%%,        (@@   @@(       @@#   .&&(   
-   @@@   @@@      (@@.            %%%   %%%        (@@   /@@,      @@@   ,@@/   
-    .%@@@&,       (@@.           .%%*   (%%,       (@@    %@@.      %@@@@@@.    
-                                 #%%     %%#                                    
-   #####%%%%%%%%%%%%%%%%%%%%%%%%%%%*     /%%    ###############((((((((((((/,   
-                                          %%#  ##*                              
-                                          *%%*##,                               
-                                           %%%#.                                
-                                           *%%                                  
-                                                                               
-                                           """)
+Funding for development of this sofware provided by NIH SPARC OT2 OD025340\n
+  -+*+-    -+++=:      -:       ====:     .-==:   
+ -@= +@:   *@::*@.    :**.      @#-=%#   .@*-=@=  
+ :%#  -.   *@  =@:    =**-      @*  *@   -@-  +-  
+   =%#:    *@==%%     *+*+      @#==%*   -@-      
+ -+  =@-   *@--:     :*:=*.     @#-#%    -@-  =:  
+ :@* *@:   *@        ++ .*-     @* :@+   :@*:-@+  
+   ---.    :-       .*-  ++     +-  -+    .=++-   
+  .......:::::::::::=*   :*:  ::::::::::........    
+                          -* +                    
+                          .*+                     
+                           .                     """)
                                                
     print("""
-The copyrights of this software are owned by Duke University.
-Please refer to the LICENSE.txt and README.txt files for licensing instructions.
-The source code can be found on the following GitHub repository: https://github.com/wmglab-duke/ascent
+The copyrights of this software are owned by Duke University
+Please refer to the LICENSE.txt and README.txt files for licensing instructions\n
+The source code can be found at the following GitHub repository: https://github.com/wmglab-duke/ascent
 ---
 """)
