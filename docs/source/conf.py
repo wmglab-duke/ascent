a791acfaf5f187b9c18ce3a8c9d5946a1dc91c83
Entirity <entirity001@gmail.com>
2021-09-17 12:01:21 -0400

    updated for markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.myst_parsers'
 ]
 
 intersphinx_mapping = {
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -9,7 +9,7 @@ Welcome to ASCENT's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
+   Home
 
 
 Indices and tables
