cfa0f232821edafb151758374452524fa11c7f6a
Daniel Marshall <daniel.p.marshall@duke.edu>
2021-09-20 14:00:04 -0400

    working local

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = 'v1.0.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme']
+extensions = ['myst_parser','sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,6 +40,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 
