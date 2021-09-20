459fd65cfed1a4ff0e4935bf69e3854f680f382b
Entirity <entirity001@gmail.com>
2021-09-20 13:06:08 -0400

    updating conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,13 +30,7 @@ release = 'v1.0.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser','sphinx_rtd_theme']
-
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'restructuredtext',
-    '.md': 'markdown',
-}
+extensions = ['sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
