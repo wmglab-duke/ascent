c7425f1e04f2cdc75ca57174ddb284d691b5c999
Daniel Marshall <daniel.p.marshall@duke.edu>
2021-09-21 11:20:05 -0400

    reset conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
 
@@ -24,20 +23,14 @@ author = 'authors_placeholder'
 # The full version, including alpha/beta/rc tags
 release = 'v1.0.3'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ["recommonmark"]
+extensions = []
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
