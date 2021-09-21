938b520dd5daa91bab8fdf4a4e897d67d8ebad56
Daniel Marshall <daniel.p.marshall@duke.edu>
2021-09-21 14:40:57 -0400

    added fiber length error message

--- src/core/fiberset.py
+++ src/core/fiberset.py
@@ -578,6 +578,7 @@ class FiberSet(Exceptionable, Configurable, Saveable):
                                                         delta_z,
                                                         x, y,
                                                         z_shift_to_center_in_model_range + z_shift_to_center_in_fiber_range)
+                    if np.any(np.array(fiber_pre)[:,2]>fiber_length): self.throw(119)
                     if diam_distribution:
                         fiber = {'diam': diam, 'fiber': fiber_pre}
                     else:
