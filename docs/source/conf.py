c8baac5e7bbe03ba430ef3dd849238368a231f59
Daniel Marshall <daniel.p.marshall@duke.edu>
2021-09-29 14:48:13 -0400

    remove staticpath

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,8 +56,3 @@ html_theme = 'sphinx_rtd_theme'
 html_show_copyright = True
 
 html_show_sphinx = False
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
