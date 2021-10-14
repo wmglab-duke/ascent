c9ed0ec5703b94fd4b7529431319c0baf7b9af7e
Daniel Marshall <daniel.p.marshall@duke.edu>
2021-10-14 11:44:08 -0400

    updated shell script

--- config/system/installation/install.sh
+++ config/system/installation/install.sh
@@ -1,26 +1,7 @@
-#! /bin/bash
-
-# set up conda for this environment, choosing between Linux/macOS
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux
-    CONDA_SETUP_SCRIPT=/hpc/group/wmglab/jec91/miniconda3/etc/profile.d/conda.bash
-    CONDA_BIN=/hpc/group/wmglab/jec91/miniconda3/envs/ascent/bin
-else
-    # macOS
-    CONDA_SETUP_SCRIPT=~/opt/miniconda3/etc/profile.d/conda.sh
-    CONDA_BIN=~/opt/miniconda3/envs/ascent/bin
-fi
-
-source $CONDA_SETUP_SCRIPT
-
 # package installation
-conda create -n ascent -y
+conda create -n ascent python==3.7 anaconda
 conda activate ascent
-conda install -y python=3.7
-"$CONDA_BIN/pip" install --upgrade pip setuptools wheel
-"$CONDA_BIN/pip" install pillow==5.2 numpy==1.16.* shapely==1.6.* matplotlib==3.2.* pyclipper==1.1.* pygame==1.9.* pymunk==5.6.* scipy==1.1.* pandas==0.25.* openpyxl==3.0.* opencv-python quantiphy scikit-image
-conda install -y shapely
-rm =*  # weird bug where pip creates empty files named "=<version>"
+pip install -r requirements.txt
 
 echo
 echo "Installation complete."
@@ -31,7 +12,6 @@ while true; do
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) echo "Not added"; exit 0;;
-        * ) echo "Not added."; exit 0;;
     esac
 done
 
