e081b1f3f49b5da1dc519cf5222a08b65a67b5a9
Daniel Marshall <daniel.p.marshall@duke.edu>
2021-10-22 14:58:18 -0400

    fixed sh

--- config/system/installation/install.sh
+++ config/system/installation/install.sh
@@ -3,7 +3,7 @@
 conda init
 CONDA_BASE=$(conda info --base)/etc/profile.d/conda.sh
 source $CONDA_BASE
-conda create -n ascent python=3.7 pip setuptools wheel
+conda create -n ascent python=3.7 pip setuptools wheel shapely
 conda activate ascent
 pip install -r requirements.txt
 
@@ -11,14 +11,16 @@ echo
 echo "Installation complete."
 echo
 # create shortcut
-while true; do
-    read -p "Add ASCENT environment setup alias to '.bash_profile'? (recommended) [y/N] " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) echo "Not added"; exit 0;;
-    esac
-done
 
-echo "alias ascent_setup='source $CONDA_SETUP_SCRIPT; conda activate ascent; cd $PWD'" >> ~/.bash_profile
-echo "Added. Remember to run 'ascent_setup' to use (requires shell restart)."
+read -p "Add ASCENT environment setup alias to '.bash_profile'? (recommended) [y/N] " yn
+case $yn in
+    [Yy]* ) 
+        echo "alias ascent_setup='source $CONDA_SETUP_SCRIPT; conda activate ascent; cd $PWD'" >> ~/.bash_profile
+        echo "Added. Remember to run 'ascent_setup' to use (requires shell restart)."
+        ;;
+    [Nn]* ) echo "Not added";;
+esac
+
+
+
 exit 0
