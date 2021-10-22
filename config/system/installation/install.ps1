92c2f962f38b72d331ac5a06adcb41fce9394c83
Daniel Marshall <daniel.p.marshall@duke.edu>
2021-10-22 14:51:30 -0400

    fixedfr

--- config/system/installation/install.ps1
+++ config/system/installation/install.ps1
@@ -1,6 +1,6 @@
 # package installation
 conda init powershell
-conda create -n ascent python=3.7 pip setuptools wheel
+conda create -n ascent python=3.7 pip setuptools wheel shapely
 conda activate ascent
 pip install -r requirements.txt
 
