110deb324057ff6c86d97ef6fb2338924d41eeea
edm23 <edm23@duke.edu>
2021-10-25 09:59:14 -0400

    Revert "Merge branch 'requirements' of https://gitlab.oit.duke.edu/wmglab/ascent"
    This reverts commit a2aae9035032a9e1c4a0551b34fb8cb98704c203, reversing
    changes made to 9010b32ec70fa5644ea708c76aa163a106dc8e5f.

--- config/system/installation/install.ps1
+++ config/system/installation/install.ps1
@@ -1,8 +1,7 @@
 # package installation
-conda init powershell
-conda create -n ascent python=3.7 pip setuptools wheel shapely
+conda create -n ascent python=3.7 pip setuptools wheel anaconda shapely scikit-image numpy==1.16.* matplotlib==3.2.* scipy==1.1.* pandas==0.25.* openpyxl==3.0.* pillow==5.2
 conda activate ascent
-pip install -r requirements.txt
+pip install pyclipper==1.1.* pygame==1.9.* pymunk==5.6.* opencv-python quantiphy
 
 # shortcut creation
 $title    = 'ASCENT Conda Environment'
@@ -10,16 +9,15 @@ $question = 'Do you want to save a shortcut for the "ascent" Conda environment t
 $choices  = '&Yes', '&No'
 $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
 if ($decision -eq 0) {
-    $condaps = $condapath + "\shell\condabin\conda-hook.ps1"
-    $Arguments = "-ExecutionPolicy ByPass -NoExit -Command `"& `'$condaps'` ; conda activate ascent; cd `'" + (Get-Item .).FullName + "`'"
+    $Arguments = "-ExecutionPolicy ByPass -NoExit -Command `"& `'C:\Users\" + $env:UserName + "\Miniconda3\shell\condabin\conda-hook.ps1`' ; conda activate ascent; cd `'" + (Get-Item .).FullName + "`'"
     $TargetFile = '%windir%\System32\WindowsPowerShell\v1.0\powershell.exe'
-    $ShortcutFile = [Environment]::GetFolderPath("Desktop")+'\ASCENT.lnk'
+    $ShortcutFile = 'ASCENT.lnk'
     $WScriptShell = New-Object -ComObject WScript.Shell
     $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
     $Shortcut.TargetPath = $TargetFile
     $Shortcut.Arguments = $Arguments
     $Shortcut.Save()
-
+    
     Write-Host 'Saved shortcut to ' $ShortcutFile
 
 } else {
