ac2d63cb705052cae52aa6de9621f0b18721b6a7
Daniel Marshall <dpm42@duke.edu>
2021-10-28 09:49:53 -0400

    Update simulation.py

--- src/core/simulation.py
+++ src/core/simulation.py
@@ -611,7 +611,8 @@ class Simulation(Exceptionable, Configurable, Saveable):
         for dirname in [f for f in os.listdir(source) if os.path.isdir(os.path.join(source, f))]:
             this_sample, this_model, this_sim, product_index = tuple(dirname.split('_'))
             if sample == int(this_sample) and model == int(this_model) and sim == int(this_sim):
-                shutil.rmtree(os.path.join(sim_dir, product_index))
+                if os.path.isdir(os.path.join(sim_dir, product_index)):
+                    shutil.rmtree(os.path.join(sim_dir, product_index))
                 shutil.copytree(os.path.join(source, dirname), os.path.join(sim_dir, product_index))
 
     def potentials_exist(self, sim_dir: str) -> bool:
