3bf3d796943d1d965539c439a16d2d37bb4cb907
Daniel Marshall <68913874+dmarshall-bing@users.noreply.github.com>
2022-02-02 12:55:44 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,12 @@ source_suffix = {
     '.md': 'markdown',
 }
 
-autodoc_mock_imports = ['numpy']
+import mock
+
+MOCK_MODULES = ['numpy']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
