eec8dc5661cebf865b825659af05e76b310c6ca4
Daniel Marshall <dpm42@duke.edu>
2022-02-16 12:41:13 -0500

    Update get_thresholds.py

--- examples/analysis/get_thresholds.py
+++ examples/analysis/get_thresholds.py
@@ -2,7 +2,7 @@
 
 """
 The copyrights of this software are owned by Duke University.
-Please refer to the LICENSE.txt and README.txt files for licensing instructions.
+Please refer to the LICENSE and README.md files for licensing instructions.
 The source code can be found on the following GitHub repository: https://github.com/wmglab-duke/ascent
 """
 
