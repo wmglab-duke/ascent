ea60c78fea418a04b67e0534c15df277b248e20b
Daniel Marshall <dpm42@duke.edu>
2022-03-15 10:22:19 -0400

    update to modelwrapper logic

--- src/model/ModelWrapper.java
+++ src/model/ModelWrapper.java
@@ -331,6 +331,8 @@ public class ModelWrapper {
         String id = ve_im.next("interp");
         model.result().numerical().create(id, "Interp");
         model.result().numerical(id).set("expr", "V");
+        model.result().numerical(id).set("recover", "ppr");
+        model.result().numerical(id).set("matherr", "on");
         model.result().numerical(id).setInterpolationCoordinates(coordinates);
         double[][][] ve_pre = model.result().numerical(id).getData();
         int len = ve_pre[0][0].length; // number of coordinates
