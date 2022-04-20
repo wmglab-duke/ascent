e7c4d91db7c1dd27c8fc29104285b460267efcc0
Daniel Marshall <dpm42@duke.edu>
2022-04-20 09:20:45 -0400

    Update runner.py

--- src/runner.py
+++ src/runner.py
@@ -474,7 +474,7 @@ class Runner(Exceptionable, Configurable):
             time.sleep(10)
             os.chdir('src')
             os.system('""{}\\javac" '
-                      '-Xlint -cp "..\\bin\\json-20190722.jar";"{}\\plugins\\*" '
+                      '-cp "..\\bin\\json-20190722.jar";"{}\\plugins\\*" '
                       'model\\*.java -d ..\\bin"'.format(jdk_path,
                                                          comsol_path))
             os.system('""{}\\java\\win64\\jre\\bin\\java" '
