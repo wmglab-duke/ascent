1346b621892ceb75bd2c611a69591864e40296f0
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-05-26 16:09:06 -0400

    analysis

--- examples/analysis/ap_time_and_loc.py
+++ examples/analysis/ap_time_and_loc.py
@@ -10,36 +10,28 @@ import os
 import sys
 
 sys.path.append(os.path.sep.join([os.getcwd(), '']))
-
+os.chdir('D:/ASCENT/fresh')
 import numpy as np
 
 import matplotlib.pyplot as plt
 from src.core.query import Query
 
 # set default fig size
-plt.rcParams['figure.figsize'] = list(np.array([16.8, 10.14*2]) / 2)
+plt.rcParams['figure.figsize'] = list(np.array([16.8, 10.14]) / 2)
 
 q = Query({
     'partial_matches': False,
     'include_downstream': True,
     'indices': {
-        'sample': [3008],
-        'model': [0,1,2,11],
-        'sim': [3001]
+        'sample': [0],
+        'model': [0],
+        'sim': [8]
     }
 }).run()
 
-q.ap_time_and_location(
-    delta_V=60,
+q.ap_loctime(
     plot=False,
     absolute_voltage=False,
-    # n_sim_label_override='7.3 µm MRG Fiber',
-    # model_labels=[
-    #     '5000 µm model radius',
-    #     '7500 µm model radius',
-    #     '10000 µm model radius',
-    # ],
-    # n_sim_filter=[0, 1, 2],
     save=True,
-    subplots = True,
-    nodes_only = True)
+    nodes_only = True,
+    amp = 0)
