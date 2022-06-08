85473aaf1016b5a7b0dbf20b229d06212ef03f32
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-06-08 09:06:31 -0400

    more change

--- docs/source/retarget.py
+++ docs/source/retarget.py
@@ -3,16 +3,17 @@ import os
 jsonlist = ['[JSON Parameters](prepend/JSON/JSON_parameters/index)',
                                            '[Sample Parameters](prepend/JSON/JSON_parameters/sample)'
                                            '[Model Parameters](prepend/JSON/JSON_parameters/model)',
-                                           '[Sim Parameters](prepend/JSON/JSON_parameters/sim)']
+                                           '[Sim Parameters](prepend/JSON/JSON_parameters/sim)',
+                                           '[Environment Parameters](prepend/JSON/JSON_parameters/env)']
 
 redict = {
     '[S8 Text](S8-JSON-file-parameter-guide)':jsonlist,
     '[S7](S7-JSON-configuration-files)':'[JSON Overview](prepend/JSON/JSON_overview)',
     '[S12](S12-Python-MockSample-class-for-creating-binary-masks-of-nerve-morphology)':'[Mock Morphology](prepend/MockSample)',
-    '[S11 Text](S11-Morphology-files)':'asd',
-    '[S26 Text](S26-Java-utility-classes)':'as',
-    '[S31](S31-NEURON-launch.hoc)':'as',
-    '[S32](S32-NEURON-Wrapper.hoc)':'as',
+    '.[S11 Text](S11-Morphology-files)':'asd',
+    '.[S26 Text](S26-Java-utility-classes)':'as',
+    '.[S31](S31-NEURON-launch.hoc)':'[Launch](prepend/Code_Hierarchy/NEURON#NEURON-Launch-hoc)',
+    '.[S32](S32-NEURON-Wrapper.hoc)':'[Wrapper](prepend/Code_Hierarchy/NEURON#NEURON-Wrapper-hoc)',
     '[S8](S8-JSON-file-parameter-guide)':jsonlist,
     '[S12 Text](S12-Python-MockSample-class-for-creating-binary-masks-of-nerve-morphology)':'[Mock Morphology](prepend/MockSample)',
     }
@@ -25,7 +26,7 @@ for path in Path('').rglob('*.md'):
         text=f.readlines()
     for i,line in enumerate(text):
         for key,value in redict.items():
-            if key in line and not key.startswith('['):
+            if key in line and key.startswith('.'):
                 print(line)
             elif key in line:
                 if type(value) is list:
