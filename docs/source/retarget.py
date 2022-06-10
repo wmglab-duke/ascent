c526c27643e770907fca5e9ed2bdb1048327e2e2
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-06-10 11:27:24 -0400

    more targetS

--- docs/source/retarget.py
+++ docs/source/retarget.py
@@ -16,7 +16,7 @@ redict = {
     '[S8 Text](S8-JSON-file-parameter-guide)':jsonlist,
 #    '[S7](S7-JSON-configuration-files)':'[JSON Overview](prepend/JSON/JSON_overview)',
     '[S7 Text](S7-JSON-configuration-files)':'[JSON Overview](prepend/JSON/JSON_overview)',
-    '[S12](S12-Python-MockSample-class-for-creating-binary-masks-of-nerve-morphology)':'[Mock Morphology](prepend/MockSample)',
+    # '[S12](S12-Python-MockSample-class-for-creating-binary-masks-of-nerve-morphology)':'[Mock Morphology](prepend/MockSample)',
     '[S11 Text](S11-Morphology-files)':'prepend/Running_ASCENT/Info#Morphology-Input-Files)',
     '[S26 Text](S26-Java-utility-classes)':'[Java Utility Classes](prepend/Code_Hierarchy/Java#Java-utility-classes)',
 #    '[S31](S31-NEURON-launch.hoc)':'[Launch](prepend/Code_Hierarchy/NEURON#NEURON-Launch-hoc)',
@@ -28,6 +28,17 @@ redict = {
     '[S17 Text](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives)':'[Creating Custom Cuffs](prepend/Primitives_and_Cuffs/Custom_Cuffs)',
     '[S18 Text](S18-Creating-new-part-primitives)':'[Creating New Part Primitives](prepend/Primitives_and_Cuffs/Creating_Primitives)',
     '[S2 Text](S2-Installation)':'[Installation](prepend/Getting_Started#Installation)',
+    '[S6 Text](S6-Enums)':'[Enums](prepend/Code_Hierarchy/Python#Enums)',
+    '[S22 Text](S22-Simulation-protocols)':'[Simulation Protocols](prepend/Running_ASCENT/Info#Simulation-Protocols)',
+    '[S13 Text](S13-Python-classes-for-representing-nerve-morphology-(Sample)':'[Python Morphology Classes](prepend/Running_ASCENT/Info#Python-classes-for-representing-nerve-morphology-(Sample))',
+    '[S33 Text](S33-Data-analysis-tools)':'[Python Morphology Classes](prepend/Running_ASCENT/Usage#Data-analysis-tools)',
+    '[S27 Text](S27-Defining-and-assigning-materials-in-COMSOL)':'[Assigning Material Properties](prepend/Running_ASCENT/Info#Defining-and-assigning-materials-in-COMSOL)',
+    '[S28 Text](S28-Definition-of-perineurium)':'[Perineurium Properties](prepend/Running_ASCENT/Info#Definition-of-perineurium)',
+    '[S25 Text](S25-Control-of-medium-surrounding-nerve-and-cuff-electrode)':'[Assigning Material Properties](prepend/Running_ASCENT/Info#Control-of-medium-surrounding-nerve and-cuff-electrode)',
+    '[S19 Text](S19-Cuff-placement-on-nerve)':'[Cuff Placement on the Nerve](prepend/Running_ASCENT/Info#Cuff-placement-on-nerve)',
+    '[S9 Text](S9-Python-utility-classes)':'[Python Utility Classes](prepend/Code_Hierarchy/Python#Python-utility-classes',
+    '[S34 Text](S34-Convergence-analysis-example)':'[Convergence Analysis Example](prepend/Convergence_Example)',
+    '[S7](S7-JSON-configuration-files) and [S8](S8-JSON-file-parameter-guide) Text':'[JSON Configuration Files](prepend/JSON/index)'
     }
 
 for path in Path('').rglob('*.md'):
@@ -37,6 +48,7 @@ for path in Path('').rglob('*.md'):
     with open(path,'r',encoding='utf8') as f:
         text=f.readlines()
     for i,line in enumerate(text):
+        replaced = False
         for key,value in redict.items():
             if key[1:] in line and key.startswith('.'):
                 print(line)
@@ -54,5 +66,8 @@ for path in Path('').rglob('*.md'):
                 newline=line.replace(key,newstr)
                 text[i]=newline
                 print('replaced')
+                replaced = True
+        if ' Text](S' in line and not replaced:
+            print(line)
     with open(path,'w',encoding='utf8') as f:
         f.writelines(text)
