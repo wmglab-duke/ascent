87b31784e63167344fcb7901f0fcfea7fa27cf7c
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-06-13 13:51:14 -0400

    more target

--- docs/source/retarget.py
+++ docs/source/retarget.py
@@ -14,6 +14,7 @@ jsonlist = ['[JSON Parameters](prepend/JSON/JSON_parameters/index)',
 
 redict = {
     '[S8 Text](S8-JSON-file-parameter-guide)':jsonlist,
+    '[S16](S16-Library-of-part-primitives-for-electrode-contacts-and-cuffs), [S17](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives), and [S18](S18-Creating-new-part-primitives) Text':'[Part Primitives and Custom Cuffs](prepend/Primitives_and_Cuffs/index)',
 #    '[S7](S7-JSON-configuration-files)':'[JSON Overview](prepend/JSON/JSON_overview)',
     '[S7 Text](S7-JSON-configuration-files)':'[JSON Overview](prepend/JSON/JSON_overview)',
     # '[S12](S12-Python-MockSample-class-for-creating-binary-masks-of-nerve-morphology)':'[Mock Morphology](prepend/MockSample)',
@@ -24,15 +25,15 @@ redict = {
 #    '[S8](S8-JSON-file-parameter-guide)':jsonlist,
     '[S12 Text](S12-Python-MockSample-class-for-creating-binary-masks-of-nerve-morphology)':'[Mock Morphology](prepend/MockSample)',
     '[S3 Text](S3-ASCENT-data-hierarchy)':'[ASCENT Data Hierarchy](prepend/Data_Hierarchy)',
-    # '[S16 Text](S16-Library-of-part-primitives-for-electrode-contacts-and-cuffs)':'[ASCENT Part Primitives](prepend/Primitives_and_Cuffs/Cuff_Primitives)',
-    # '[S17 Text](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives)':'[Creating Custom Cuffs](prepend/Primitives_and_Cuffs/Custom_Cuffs)',
-    # '[S18 Text](S18-Creating-new-part-primitives)':'[Creating New Part Primitives](prepend/Primitives_and_Cuffs/Creating_Primitives)',
+    '[S16 Text](S16-Library-of-part-primitives-for-electrode-contacts-and-cuffs)':'[ASCENT Part Primitives](prepend/Primitives_and_Cuffs/Cuff_Primitives)',
+    '[S17 Text](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives)':'[Creating Custom Cuffs](prepend/Primitives_and_Cuffs/Custom_Cuffs)',
+    '[S18 Text](S18-Creating-new-part-primitives)':'[Creating New Part Primitives](prepend/Primitives_and_Cuffs/Creating_Primitives)',
     '[S2 Text](S2-Installation)':'[Installation](prepend/Getting_Started.md#installation)',
     'Enums ([S6 Text](S6-Enums))':'[Enums](prepend/Code_Hierarchy/Python.md#enums)',
     'Enums, [S6 Text](S6-Enums)':'[Enums](prepend/Code_Hierarchy/Python.md#enums),',
     '([S6 Text](S6-Enums))':'([Enums](prepend/Code_Hierarchy/Python.md#enums))',
     '[S22 Text](S22-Simulation-protocols)':'[Simulation Protocols](prepend/Running_ASCENT/Info.md#simulation-protocols)',
-    '[S13 Text](S13-Python-classes-for-representing-nerve-morphology-(Sample)':'[Python Morphology Classes](prepend/Running_ASCENT/Info.md#python-classes-for-representing-nerve-morphology-(sample))',
+    '[S13 Text](S13-Python-classes-for-representing-nerve-morphology-(Sample)':'[Python Morphology Classes](prepend/Code_Hierarchy/Python.md#python-classes-for-representing-nerve-morphology-sample)',
     '[S33 Text](S33-Data-analysis-tools)':'[Python Morphology Classes](prepend/Running_ASCENT/Usage.md#data-analysis-tools)',
     '[S27 Text](S27-Defining-and-assigning-materials-in-COMSOL)':'[Assigning Material Properties](prepend/Running_ASCENT/Info.md#defining-and-assigning-materials-in-comsol)',
     '[S28 Text](S28-Definition-of-perineurium)':'[Perineurium Properties](prepend/Running_ASCENT/Info.md#definition-of-perineurium)',
@@ -41,8 +42,7 @@ redict = {
     '[S9 Text](S9-Python-utility-classes)':'[Python Utility Classes](prepend/Code_Hierarchy/Python.md#python-utility-classes',
     '[S34 Text](S34-Convergence-analysis-example)':'[Convergence Analysis Example](prepend/Convergence_Example)',
     '[S7](S7-JSON-configuration-files) and [S8](S8-JSON-file-parameter-guide) Text':'[JSON Configuration Files](prepend/JSON/index)',
-    # '[S17 text](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives)':'[Creating Custom Cuffs](prepend/Primitives_and_Cuffs/Custom_Cuffs)',
-    # '[S17](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives)':'[Creating Custom Cuffs](prepend/Primitives_and_Cuffs/Custom_Cuffs)',
+    '[S17 text](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives)':'[Creating Custom Cuffs](prepend/Primitives_and_Cuffs/Custom_Cuffs)',
     '[S19](S19-Cuff-placement-on-nerve) Text':'[Cuff Placement on the Nerve](prepend/Running_ASCENT/Info.md#cuff-placement-on-nerve)',
     '["ASCENT data hierarchy"](S3-ASCENT-data-hierarchy)':'[ASCENT Data Hierarchy](prepend/Data_Hierarchy)',
     '.(S18-Creating-new-part-primitives)':'[Creating New Part Primitives](prepend/Primitives_and_Cuffs/Creating_Primitives)',
@@ -78,3 +78,5 @@ for path in Path('').rglob('*.md'):
             print(line)
     with open(path,'w',encoding='utf8') as f:
         f.writelines(text)
+
+redict['[S17](S17-Creating-custom-preset-cuffs-from-instances-of-part-primitives)']='[Creating Custom Cuffs](prepend/Primitives_and_Cuffs/Custom_Cuffs)'
