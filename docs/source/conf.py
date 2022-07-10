9c63e94b4a2cdc334df75bac5702eafd4cd86f5a
Daniel Marshall <daniel.p.marshall@duke.edu>
2022-07-10 11:30:10 -0400

    apply precommit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,14 +31,14 @@ release = 'v1.1.2'
 # ones.
 
 extensions = ['myst_parser',
-            'sphinxarg.ext',
-            'sphinx.ext.intersphinx',
-            'sphinx.ext.autodoc',
-            'sphinxcontrib.bibtex',
-            'sphinxcontrib.details.directive',
-            'sphinx_copybutton',
-            'sphinx_rtd_dark_mode',
-             'sphinx_github_changelog']
+              'sphinxarg.ext',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.autodoc',
+              'sphinxcontrib.bibtex',
+              'sphinxcontrib.details.directive',
+              'sphinx_copybutton',
+              'sphinx_rtd_dark_mode',
+              'sphinx_github_changelog']
 
 source_suffix = {
     '.rst': 'restructuredtext',
