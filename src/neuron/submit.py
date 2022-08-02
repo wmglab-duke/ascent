244624c50851a1c1a2bf24cf48831a5e76eaf681
Daniel Marshall <dpm42@duke.edu>
2022-08-02 15:14:08 -0400

    Update submit.py

--- src/neuron/submit.py
+++ src/neuron/submit.py
@@ -4,7 +4,7 @@
 The copyrights of this software are owned by Duke University.
 Please refer to the LICENSE and README.md files for licensing instructions.
 The source code can be found on the following GitHub repository: https://github.com/wmglab-duke/ascent
-1"""
+"""
 
 import argparse
 import json
